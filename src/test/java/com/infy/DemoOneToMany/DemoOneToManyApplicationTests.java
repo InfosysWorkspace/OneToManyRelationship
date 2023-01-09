bf53f9bb366c9719805c6997a3096ae2a744ee58
BsoftMaewada <maewada@live.com>
2023-01-09 04:41:15 -0500

    initial commit

---
+++ src/test/java/com/infy/DemoOneToMany/DemoOneToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.DemoOneToMany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOneToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
